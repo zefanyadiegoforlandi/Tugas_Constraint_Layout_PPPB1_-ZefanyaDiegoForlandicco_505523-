f6f11e39d22b7e29f461a5775c3008778068f323
unknown <zddiiego@gmail.com>
2023-09-12 22:32:38 +0700

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ConstraintLayout(ZefanyaDiegoF_505523)"
+include(":app")
+ 
